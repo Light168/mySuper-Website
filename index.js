c975575f63d25f1c76004e92051b07de72879f0b
Light168 <89193576+Light168@users.noreply.github.com>
2024-06-12 08:04:37 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const fullName="Broseph Code";
+let firstName=fullName.slice(0,fullName.indexOf(" "));
+console.log(firstName);
+let lastName=fullName.slice(fullName.indexOf(" "));
+console.log(lastName);
